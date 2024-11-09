a6e999af4a1e96f71ac647d6536f92427dbb8f58
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-11-09 23:20:54 +0600

    added initial templates

--- book/models.py
+++ book/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    image1 = models.ImageField(upload_to='/media/book/image',blank=True,null=True)
-    image2 = models.ImageField(upload_to='/media/book/image',blank=True,null=True)
-    image3 = models.ImageField(upload_to='/media/book/image',blank=True,null=True)
+    image1 = models.ImageField(upload_to='media/book/images',blank=True,null=True)
+    image2 = models.ImageField(upload_to='media/book/images',blank=True,null=True)
+    image3 = models.ImageField(upload_to='media/book/images',blank=True,null=True)
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=150,null=True,blank=True)
     price = models.DecimalField(max_digits=6,decimal_places=2)
